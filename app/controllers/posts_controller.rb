d452269128fafd18f195f747a05df333fccc5f01
Ángel Díaz <diazangel9311@gmail.com>
2021-07-13 18:23:04 -0500

    Add relation between user and post models

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -66,6 +66,6 @@ class PostsController < ApplicationController
 
     # Only allow a list of trusted parameters through.
     def post_params
-      params.require(:post).permit(:post)
+      params.require(:post).permit(:post, :user_id)
     end
 end
